2d2d66d90a5f1f309657f02dcb715cae45ddfbda
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2025-04-21 19:33:42 +0200

    feat: create snowflake client and semantic model manager accessible to all modules with package initialization

---
+++ cafe/__init__.py
@@ -0,0 +1,5 @@
+from cafe.core.semantic_model import SemanticModelManager
+from cafe.core.snowflake_client import SnowflakeClient
+
+snowflake_client = SnowflakeClient()
+semantic_model_manager = SemanticModelManager()
