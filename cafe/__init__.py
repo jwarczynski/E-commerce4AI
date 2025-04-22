5fccfe7d860ab88c158b2c299af942b2c20a58cc
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2025-04-22 23:24:09 +0200

    feat: use judge agent as a tool for main agent

--- cafe/__init__.py
+++ cafe/__init__.py
@@ -2,4 +2,4 @@ from cafe.core.semantic_model import SemanticModelManager
 from cafe.core.snowflake_client import SnowflakeClient
 
 snowflake_client = SnowflakeClient()
-semantic_model_manager = SemanticModelManager()
+semantic_model_manager = SemanticModelManager(snowflake_client)
